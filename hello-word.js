39f88c3690144d3494049e8862bad12c92d202c4
santiagob123 <santobernalvega@gmail.com>
2024-11-20 14:36:15 -0500

    Confirmando cambios tras la instalación de nodemon

--- hello-word.js
+++ hello-word.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000; // Cambiado de 300 a 3000
+const port = 300;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
@@ -9,10 +9,7 @@ const server = http.createServer((req, res) => {
     res.end('Hello, world!\n');
 });
 
-// El siguiente bloque inicia el servidor
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
-Page 10 of 71
-Luego actualice los cambios en el repositorio local
-git commit -a -m ‘nodemon instalado
+

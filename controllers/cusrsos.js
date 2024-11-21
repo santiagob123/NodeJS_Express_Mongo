38594c9bef232ba11d53593eda4cd352d7cdd0b5
Deibyth <164409835+DiegoEsteban10@users.noreply.github.com>
2024-11-21 08:00:49 -0500

    adelanto de cursos y usuarios de la carpeta controller

---
+++ controllers/cusrsos.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const Curso = require('../models/curso_model'); // Asumiendo la ruta del modelo
+const ruta = express.Router();
+
+module.exports = ruta;
---
+++ controllers/usuarios.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const Usuario = require('../models/usuario_model'); // Asumiendo la ruta del modelo
+const ruta = express.Router();
+
+module.exports = ruta;

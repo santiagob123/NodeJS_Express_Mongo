5d5910b9a731d96a0864dfe3da7960ede80f48d1
santiagob123 <santobernalvega@gmail.com>
2024-11-22 07:47:09 -0500

    Estructurando el proyecto API REST. Index, modelos y controladores

--- controllers/cusrsos.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const Curso = require('../models/curso_model'); // Asumiendo la ruta del modelo
-const ruta = express.Router();
-
-module.exports = ruta;
--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+// Importación de controladores
+const usuarios = require('./controllers/usuarios');
+const cursos = require('./controllers/cursos');
+
 const express = require('express');
 const mongoose = require('mongoose');
 
 // Conexión a la base de datos MongoDB
-mongoose.connect('mongodb://localhost:27017/usersCourseDB', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/usuarioscursosdb', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Conectado a MongoDB...'))
-    .catch(err => console.log('No se pudo conectar con MongoDB.', err));
+    .catch(err => console.log('No se pudo conectar con MongoDB...', err));
 
 // Middleware
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Endpoints (recursos)
+app.use('/api/usuarios', usuarios);
+app.use('/api/cursos', cursos);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`API REST OK y ejecutándose en el puerto ${port}...`);
+    console.log(`API REST OK, y ejecutándose en el puerto ${port}...`);
 });
